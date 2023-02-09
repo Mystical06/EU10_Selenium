14c2c5cae5cb2d564deda26afc9b2252d3145b27
Mustafa Aydin <97643186+Mystical06@users.noreply.github.com>
2023-02-09 13:32:08 +0000

    Day11-Scroll

--- src/test/java/com/cydeo/tests/day11_actions_jsexecutor_practice/T1_Circle_Drag_N_Drop.java
+++ src/test/java/com/cydeo/tests/day11_actions_jsexecutor_practice/T1_Circle_Drag_N_Drop.java
@@ -1,10 +1,11 @@
 package com.cydeo.tests.day11_actions_jsexecutor_practice;
 
-import com.cydeo.tests.utilities.*;
-import org.openqa.selenium.*;
-import org.openqa.selenium.interactions.*;
-import org.testng.*;
-import org.testng.annotations.*;
+import com.cydeo.tests.utilities.BrowserUtils;
+import com.cydeo.tests.utilities.Driver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
 
 public class T1_Circle_Drag_N_Drop {
 
@@ -14,30 +15,23 @@ public class T1_Circle_Drag_N_Drop {
         Driver.getDriver().get("https://demos.telerik.com/kendo-ui/dragdrop/index");
 
         //Locate "Accept Cookies" button and click
-        WebElement acceptCookiesButton = Driver.getDriver().findElement(By.id("onetrust-accept-btn-handler"));
-        acceptCookiesButton.click();
+        Driver.getDriver().findElement(By.id("onetrust-accept-btn-handler")).click();
+
 
         //Locate small and big circles to be able to drag them around
         WebElement smallCircle = Driver.getDriver().findElement(By.id("draggable"));
         WebElement bigCircle = Driver.getDriver().findElement(By.id("droptarget"));
 
         //2. Drag and drop the small circle to bigger circle.
-        Actions actions = new Actions(Driver.getDriver());
+        JavascriptExecutor js= (JavascriptExecutor) Driver.getDriver();
+        BrowserUtils.sleep(2);
+
         //actions.dragAndDrop(smallCircle, bigCircle).perform();
-        actions.clickAndHold(smallCircle)
-                .pause(2000)
-                .moveToElement(bigCircle)
-                .pause(2000)
-                .release()
-                .perform();
+
 
         //3. Assert:
         //-Text in big circle changed to: “You did great!”
 
-        String actualBigCircleText = bigCircle.getText();
-        String expectedBigCircleText = "You did great!";
-
-        Assert.assertEquals(actualBigCircleText, expectedBigCircleText);
     }
 
 
--- src/test/java/com/cydeo/tests/day11_actions_jsexecutor_practice/T7_Scroll_JSExecutor.java
+++ src/test/java/com/cydeo/tests/day11_actions_jsexecutor_practice/T7_Scroll_JSExecutor.java
@@ -1,7 +1,9 @@
 package com.cydeo.tests.day11_actions_jsexecutor_practice;
 
+import com.cydeo.tests.utilities.BrowserUtils;
 import com.cydeo.tests.utilities.Driver;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
 
@@ -13,18 +15,30 @@ public class T7_Scroll_JSExecutor {
         //2- Go to: https://practice.cydeo.com/large
         Driver.getDriver().get("https://practice.cydeo.com/large");
 
-        //Down-casting our driver type to JavascriptExecutor,
-        // so we are able to use the methods coming from that interface
-
-
-        //3- Scroll down to “Cydeo” link
         WebElement cydeoLink = Driver.getDriver().findElement(By.linkText("CYDEO"));
         WebElement homeLink= Driver.getDriver().findElement(By.linkText("Home"));
 
 
-        //4- Scroll up to “Home” link
+        //Down-casting our driver type to JavascriptExecutor,
+        // so we are able to use the methods coming from that interface
+        JavascriptExecutor js = (JavascriptExecutor) Driver.getDriver();
 
+
+
+
+        //3- Scroll down to “Cydeo” link
+        BrowserUtils.sleep(2);
+        js.executeScript("arguments[0].scrollIntoView(true)", cydeoLink);
+
+
+        //4- Scroll up to “Home” link
+        BrowserUtils.sleep(2);
+        js.executeScript("arguments[0].scrollIntoView(true)",homeLink);
         //js.executeScript("arguments[1].scrollIntoView(true)", cydeoLink, homeLink);
+
+
+        BrowserUtils.sleep(2);
+        Driver.closeDriver();
 
 
         //objects
